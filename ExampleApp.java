b17d9cedb558a61346f58bde6ffe61e5db310471
kasprzakewa <272356@student.pwr.edu.pl>
2024-04-07 16:45:00 +0200

    added example app

---
+++ ExampleApp.java
@@ -0,0 +1,34 @@
+package com.ocado;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.ocado.basket.BasketSplitter;
+
+public class ExampleApp 
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter config file path: ");
+        String configFilePath = System.console().readLine();
+        BasketSplitter splitter = new  BasketSplitter(configFilePath);
+
+        List<String> items;
+        System.out.print("Enter basket file path: ");
+        String basketFilePath = System.console().readLine();
+
+        try 
+        {
+            items = splitter.load_basket(basketFilePath);
+            Map<String, List<String>> splittedItems = splitter.split(items);
+            splitter.saveBasket(splittedItems);
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        } 
+
+        System.out.println("Basket has been split and saved to file output.json.");
+    } 
+}
